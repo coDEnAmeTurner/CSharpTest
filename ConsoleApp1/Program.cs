54ad97bc868be762cfe5726cd655d7b1c395f504
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-07-11 23:32:06 +0700

    testing dataset

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,43 +1,24 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
-const string connectionString =
-            "Persist Security Info=False;User ID=sa;Password=Admin@123;Initial Catalog=TripBooking;Server=ADMIN\\DOTNETTEST";
+ConnectionStringSettings settings =
+            ConfigurationManager.ConnectionStrings["TripBooking"];
 
-// Provide the query string with a parameter placeholder.
-const string queryString =
-    "SELECT * from dbo.[User]"
-        + " WHERE userName like @name+'%'";
+string connectionString = settings.ConnectionString;
 
-// Specify the parameter value.
-const string name = "Mai";
+string queryString = "Select * from [User]";
 
-// Create and open the connection in a using block. This
-// ensures that all resources will be closed and disposed
-// when the code exits.
 using (SqlConnection connection =
     new(connectionString))
 {
-    // Create the Command and Parameter objects.
-    SqlCommand command = new(queryString, connection);
-    command.Parameters.AddWithValue("@name", name);
-
-    // Open the connection in a try/catch block.
-    // Create and execute the DataReader, writing the result
-    // set to the console window.
-    try
+    SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+    DataSet users = new DataSet();
+    adapter.Fill(users, "Users");  
+    var table = users.Tables["Users"];
+    foreach (DataRow row in table.Rows)
     {
-        connection.Open();
-        SqlDataReader reader = command.ExecuteReader();
-        while (reader.Read())
-        {
-            Console.WriteLine($"\t{reader[0]}\t{reader[1]}\t{reader[2]}");
-        }
-        reader.Close();
+        Console.WriteLine($"UserId: {row["id"]}, UserName: {row["userName"]}");
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-    Console.ReadLine();
 }
